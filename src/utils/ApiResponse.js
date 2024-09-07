b0298020fa89dfe319379ae64a19239d7d9a56c1
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-09-07 09:27:01 +0530

    sum bug fixes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,6 +1,5 @@
 class ApiResponse{
     constructor(statusCode,message="success",data){
-        super(message);
         this.statusCode=statusCode;
         this.data=data;
         this.success=statusCode<400;
